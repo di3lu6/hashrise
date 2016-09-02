aa25e5b6afd3cfe33fdf5100f7060ae5c963fe81
Guillermo Rodriguez <guillermo85@gmail.com>
2016-09-02 10:38:26 -0300

    Added micro spa, created some new views

---
+++ data/services.js
@@ -0,0 +1,18 @@
+var services = [
+    {
+        "id": 0,       
+        "isActive": true,
+        //"firstName": "Ellis",
+        //"lastName": "Yates",
+        //"company": "Xumonk",
+        //"email": "ellisyates@xumonk.com",
+        "name": "Penetration testing",
+        "description": "Nuestro servicio de pruebas de penetración, involucra un análisis profundo a los activos de información por potenciales riesgos que pudieran impactar su organización. El análisis simula un cyber ataque real, desde la posición de un potencial atacante, y puede involucrar aprovecharse activamente de las vulnerabildiades de seguridad. Trabajamos de cerca con usted para identificar y mitigar las áreas de posibles riesgos. Todos nuestros hallazgos serán presentados a la organización, conocerá vectores de ataques, amenazas, impactos y metodos de remediación técnicos. El Pentest le ayudará en:        Proactivamente cuantificar y reducir riesgos de negocio   Validar la efectividad de sus políticas de seguridad   Protejer la reputación de su marca y mantener la confianza de clientes   Prevenir inactividad, juicios y pérdidas monetarias   Mantener cumplimiento de normativas internacionales de seguridad   Obtener informes para priorizar y reducir riesgos.",
+        "tags": [
+            "exercitation",
+            "seller",
+            "minim"
+        ],
+        "popularity": 0
+    }
+];
